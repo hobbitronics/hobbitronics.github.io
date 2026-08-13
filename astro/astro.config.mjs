675ddaab43c19b5a467e9f671a8af8cd31715a56
Michael Wilson <miguel.dw@gmail.com>
2026-08-13 21:49:04 +0930

    Fix TypeScript errors and rebuild

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://hobbitronics.github.io',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx()],
 });
